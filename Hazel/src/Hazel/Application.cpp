222e8800b55d3219b9c8129405ef5bd221acf8f5
Stephanie Phillips <79615185+AllegroVivo@users.noreply.github.com>
2023-08-08 19:44:49 -0700

    Window Abstraction & GLFW (Video #11) complete!

---
+++ Hazel/src/Hazel/Application.cpp
@@ -0,0 +1,29 @@
+﻿#include "hzpch.h"
+#include "Application.h"
+
+#include "Hazel/Events/ApplicationEvent.h"
+#include "Hazel/Log.h"
+
+#include <GLFW/glfw3.h>
+
+namespace Hazel
+{
+    Application::Application()
+    {
+        m_Window = std::unique_ptr<Window>(Window::Create());
+    }
+
+    Application::~Application()
+    {
+    }
+
+    void Application::Run()
+    {
+        while (m_running)
+        {
+            glClearColor(1, 0, 1, 1);
+            glClear(GL_COLOR_BUFFER_BIT);
+            m_Window->OnUpdate();            
+        }
+    }
+}
---
+++ Hazel/src/Hazel/Application.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "Core.h"
+#include "Window.h"
+#include "Events/Event.h"
+
+namespace Hazel
+{
+    class HAZEL_API Application
+    {
+    public:
+        Application();
+        virtual ~Application();
+
+        void Run();
+    private:
+        std::unique_ptr<Window> m_Window;
+        bool m_running = true;
+    };
+
+    Application* CreateApplication();
+}
